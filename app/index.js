64f36dd3058aea7117e56887d57205cf10008c1d
Eduar A. Yanez <educartoons@gmail.com>
2016-08-20 10:43:20 -0500

    Using container functions in React.js

--- app/index.js
+++ app/index.js
@@ -9,22 +9,22 @@ var routes = require('./config/routes');
 var Raven = require('raven-js');
 
 
-var sentryKey = '0d5a683b20ba4d188f6fb8d81ead92e6';
-var sentryApp = '93244';
-var sentryURL = 'https://' + sentryKey + '@app.getsentry.com/' + sentryApp;
-
-var _APP_INFO = {
-  name: 'Github Battle',
-  branch: '4',
-  version: '1.0'
-};
-
-Raven.config(sentryURL, {
-  release: _APP_INFO.version,
-  tags: {
-    branch: _APP_INFO.branch,
-  }
-}).install();
+// var sentryKey = '0d5a683b20ba4d188f6fb8d81ead92e6';
+// var sentryApp = '93244';
+// var sentryURL = 'https://' + sentryKey + '@app.getsentry.com/' + sentryApp;
+//
+// var _APP_INFO = {
+//   name: 'Github Battle',
+//   branch: '4',
+//   version: '1.0'
+// };
+//
+// Raven.config(sentryURL, {
+//   release: _APP_INFO.version,
+//   tags: {
+//     branch: _APP_INFO.branch,
+//   }
+// }).install();
 
 ReactDOM.render(
   routes,
